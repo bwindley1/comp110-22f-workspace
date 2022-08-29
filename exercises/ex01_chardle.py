d6ac11e826dacbf60fb9b683013304df8a603669
Brent Holland <bwindley@unc.edu>
2022-08-29 15:47:33 -0400

    Chardle Exercise

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,8 +2,15 @@
 
 __author__ = "730568755"
 
-five_char: str = input("Enter a 5-character word: " )
-one_char: str = input("Enter a single character: " )
+five_char: str = input("Enter a 5-character word: ")
+if len(five_char) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+
+one_char: str = input("Enter a single character: ")
+if len(one_char) != 1:
+    print("Error: Character must be a single character.")
+    exit()
 match: int = 0
 
 print("Searching for " + one_char + " in " + five_char)
@@ -23,15 +30,13 @@ if one_char == five_char[3]:
 if one_char == five_char[4]:
     print(one_char + " found at index 4")
     match = match + 1
+
 if match == 0:
     print("No instances of " + one_char + " found in " + five_char)
-else:
+if match > 1:
+    print(str(match) + " instances of " + one_char + " found in " + five_char)
+if match == 1:
     print(str(match) + " instance of " + one_char + " found in " + five_char)
 
-
-
-
-
-
-
-
+if len(five_char) != 5:
+    print("Error: Word must contain 5 characters")
